3a3e1df161d8ed337b47f3f49c6618bfa31f7b65
Vinicius Lima <viniciuslima.fsa@gmail.com>
2021-08-10 11:14:30 -0300

    test: should get space media entity for a given date from the repository

---
+++ lib/features/domain/entities/space_media_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class SpaceMediaEntity extends Equatable {
+  final String explanation;
+  final String mediaType;
+  final String title;
+  final String url;
+
+  SpaceMediaEntity({
+    required this.explanation,
+    required this.mediaType,
+    required this.title,
+    required this.url,
+  });
+
+  @override
+  List<Object> get props => [
+        explanation,
+        mediaType,
+        title,
+        url,
+      ];
+}
---
+++ test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
+import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
+import 'package:nasa_clean_arch/features/domain/usecases/get_space_media_from_date_usecase.dart';
+
+class MockSpaceMediaRepository extends Mock implements ISpaceMediaRepository {}
+
+void main() {
+  late GetSpaceMediaFromDateUsecase usecase;
+  late ISpaceMediaRepository repository;
+
+  setUp(() {
+    repository = MockSpaceMediaRepository();
+    usecase = GetSpaceMediaFromDateUsecase(repository);
+  });
+
+  final tSpaceMedia = SpaceMediaEntity(
+      explanation:
+          'rthlings typically watch meteor showers by looking up. But this remarkable view, captured on August 13, 2011 by astronaut Ron Garan, caught a Perseid meteor by looking down. From Garan\'s perspective onboard the International Space Station orbiting at an altitude of about 380 kilometers, the Perseid meteors streak below, swept up dust left from comet Swift-Tuttle heated to incandescence. The glowing comet dust grains are traveling at about 60 kilometers per second through the denser atmosphere around 100 kilometers above Earth\'s surface. In this case, the foreshortened meteor flash is right of frame center, below the curving limb of the Earth and a layer of greenish airglow, just below bright star Arcturus. Want to look up at a meteor shower?  You\'re in luck, as the 2021 Perseids meteor shower peaks this week. This year, even relatively faint meteors should be visible through clear skies from a dark location as the bright Moon will mostly absent.   Notable Perseids Submissions to APOD: 2018, 2019, 2020',
+      mediaType: 'image',
+      title: ' Perseid Below',
+      url: 'https://apod.nasa.gov/apod/image/2108/perseid_iss_1080.jpg');
+
+  final tDate = DateTime(2021, 8, 8);
+
+  test(
+    'should get space media entity for a given date from the repository',
+    () async {
+      when(() => repository.getSpaceMediaFromDate(tDate))
+          .thenAnswer((_) async => Right(tSpaceMedia));
+
+      final result = await usecase(tDate);
+      expect(result, Right(tSpaceMedia));
+      verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
+    },
+  );
+}
